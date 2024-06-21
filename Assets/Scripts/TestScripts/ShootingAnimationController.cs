97fee8635a84c9b9775b0fddb65962f9a95d96b8
sonflea <dhk_1@naver.com>
2024-06-22 05:10:56 +0900

    animation update

--- Assets/Scripts/TestScripts/ShootingAnimationController.cs
+++ Assets/Scripts/TestScripts/ShootingAnimationController.cs
@@ -13,11 +13,13 @@ public class ShootingAnimationController : MonoBehaviour
 
     private Coroutine attackCoroutine;
     private float angle;
+    private SpriteRenderer body;
     private SpriteRenderer head;
     private SpriteRenderer arm;
 
     private void Start()
     {
+        body = GetComponent<SpriteRenderer>();
         arm = bodys[0].transform.GetChild(0).GetComponent<SpriteRenderer>();
         head = bodys[1].GetComponent<SpriteRenderer>();
     }
@@ -33,11 +35,21 @@ public class ShootingAnimationController : MonoBehaviour
         screenPoint.z = 0;
         var resultPoint = screenPoint - thisPoint;
         angle = Quaternion.FromToRotation(Vector2.down, resultPoint).eulerAngles.z;
-        headAnim.SetFloat("angle", angle);
+        headAnim.SetFloat("angle", Vector2.Angle(Vector2.down, resultPoint));
         var flip = Mathf.Sign(resultPoint.x) >= 0 ? false : true;
         arm.flipY = flip;
         head.flipX = flip;
         bodys[0].transform.localEulerAngles = new Vector3(0, 0, angle - 90);
+    }
+
+    private void LateUpdate()
+    {
+        if(body.flipX) foreach(var body in bodys)
+        {
+            var temp = body.transform.localPosition;
+            temp.x = -temp.x;
+            body.transform.localPosition = temp;
+        }
     }
 
     public void AttackAni()
@@ -48,7 +60,7 @@ public class ShootingAnimationController : MonoBehaviour
     private IEnumerator Attacking()
     {
         SpriteAssetChange(1);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(10000000f);
         SpriteAssetChange(0);
         attackCoroutine = null;
     }
