ba687d9935285ed678818e8baa51fa80e3b76587
sonflea <dhk_1@naver.com>
2024-07-14 07:11:20 +0900

    asdf

--- Assets/Scripts/TestScripts/Manager/CameraManager.cs
+++ Assets/Scripts/TestScripts/Manager/CameraManager.cs
@@ -12,21 +12,11 @@ public class CameraManager : MonoBehaviour
     public ProCamera2DPointerInfluence proCamera2DPointerInfluence;
 
     public float ChangeSize { set => ProCamera2D.Instance.UpdateScreenSize(value, 0.5f); }
-    // public float CameraOffsetY { set => cameraOffsetY = value; }
 
     void Awake()
     {
         if(instance == null) instance = this;
         proCamera2DShake = Camera.main.GetComponent<ProCamera2DShake>();
         proCamera2DPointerInfluence = Camera.main.GetComponent<ProCamera2DPointerInfluence>();
-    }
-
-    void Update()
-    {
-        // ProCamera2D.Instance.OffsetY = cameraOffsetY;
-        // if(!ChangeSize.Equals(Camera.main.orthographicSize))
-        // {
-        //     Camera.main.orthographicSize = Mathf.Ceil(Mathf.Lerp(Camera.main.orthographicSize, ChangeSize, 5f * Time.deltaTime) * 10000) * 0.0001f;
-        // }
     }
 }
