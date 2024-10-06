c986c29d386125d5d661e2f743e6e781217b978a
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-10-06 23:48:22 +0900

    테스트중

--- Assets/Scripts/TestScripts/Timeline/LoopController.cs
+++ Assets/Scripts/TestScripts/Timeline/LoopController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class LoopController : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class LoopController : MonoBehaviour
     [SerializeField] private PlayableDirector director;
 
 
+
     public void SetNone()
     {
         this.playableDirector.extrapolationMode = DirectorWrapMode.None;
@@ -19,21 +21,38 @@ public class LoopController : MonoBehaviour
     public void SetLoop(PlayableDirector dir)
     {
         Debug.Log($"SetLoop - Current Frame: {playableDirector.time}");
+        double timeLineT = playableDirector.time;
         LoopDir = dir;
 
         //playableDirector.time=
-        playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(0);
 
-        playableDirector.Pause();
-        LoopDir.Play();
+        var a =playableDirector.duration;
+        Debug.Log("길이 " + a);
+        //SetNone();
+        playableDirector.playableGraph.GetRootPlayable(0).SetDuration(timeLineT);
+        Debug.Log("테스트 후 길이 " + a);
+        //playableDirector.Pause();
+        //LoopDir.Play();
 
     }
+    //public override double duration
+    //{
+    //    get
+    //    {
+    //        if (playableDirector == null)
+    //            return base.duration;
+    //
+    //        // use this instead of length to avoid rounding precision errors,
+    //        return (double)m_Clip.samples / (double)m_Clip.frequency;
+    //    }
+    //}
 
     public void EndLoop()
     {
         LoopDir.Stop();
-        playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(1);
-        playableDirector.Resume();
+        //playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(1);
+        playableDirector.playableGraph.GetRootPlayable(0).SetDuration(23.116666666666);
+        //playableDirector.Resume();
     }
 
     // Start is called before the first frame update
