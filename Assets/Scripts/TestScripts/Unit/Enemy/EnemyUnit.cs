83987cf124dabd84a13b703621be5e2091a95829
heyhobeach <zxc120021@gmail.com>
2024-11-14 15:04:32 +0900

    Update EnemyUnit.cs

--- Assets/Scripts/TestScripts/Unit/Enemy/EnemyUnit.cs
+++ Assets/Scripts/TestScripts/Unit/Enemy/EnemyUnit.cs
@@ -59,6 +59,7 @@ public class EnemyUnit : UnitBase, IDamageable
     public override void StopAllC()
     {
     }
+
     public void DeathFeedBack(Vector2 dir)
     {
         throw new System.NotImplementedException();
