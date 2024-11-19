934150d2ade6752f8959af172452216624c49954
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-11-19 22:46:16 +0900

    ..

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -10,33 +10,34 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public CompositeCollider2D compositeCollider2D;
 
     public ContactFilter2D contactFilter; // 필터 설정
-    public List<ContactPoint2D> colliders;
-
+    public List<Collider2D> colliders;
+    public List<ContactPoint2D> contacts;
 
     // Update is called once per frame
     void Update()
     {
-        //int count = compositeCollider2D.GetContacts(colliders);
-        ////bool count =compositeCollider2D.OverlapPoint()
-        //if (count == 0)
-        //{
-        //    Debug.Log("발견된것이 업음");
-        //}
-        //if (count > 0)
-        //{
-        //    Debug.Log("겹친 콜라이더의 수: " + count);
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        Debug.Log("겹친 객체: " + colliders[i].point);
-        //    }
-        //}
+        int count = compositeCollider2D.GetContacts(colliders);
+        //bool count =compositeCollider2D.OverlapPoint()
+        if (count == 0)
+        {
+            Debug.Log("발견된것이 업음");
+        }
+        if (count > 0)
+        {
+            Debug.Log("겹친 콜라이더의 수: " + count);
+            for (int i = 0; i < count; i++)
+            {
+                Debug.Log("겹친 객체: " + colliders[i].bounds.center);
+            }
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
 
             foreach (ContactPoint2D contact in collision.contacts)
             {
+                //contacts.Add(contact);
                 Debug.Log("Contact Point: " + contact.point+"name is"+collision.transform.name);
             }
 
