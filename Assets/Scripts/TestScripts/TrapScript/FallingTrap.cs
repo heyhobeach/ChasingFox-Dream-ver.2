f86d0ccdf88f491ddf669787f4e3d0ac1b66b51a
heyhobeach <zxc120021@gmail.com>
2024-11-26 17:34:34 +0900

    함정오브젝트 생성

--- Assets/Scripts/TestScripts/TrapScript/FallingTrap.cs
+++ Assets/Scripts/TestScripts/TrapScript/FallingTrap.cs
@@ -25,10 +25,30 @@ public class FallingTrap : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(collision.gameObject.name + "이름");
         if (joint2D != null)
         {
             joint2D.useConnectedAnchor = false;
             joint2D.connectedBody = null;
+        }
+        if(joint2D!= null && collision.gameObject.layer == LayerMask.NameToLayer("Map"))//오브젝트 접점 위치 판단을 위해
+        {
+            Vector2 vec2 = this.transform.position;
+            GameObject gobj = transform.parent.GetComponent<TrapSet>().TrapWreck;
+            //GameManager.Instance.
+            ContactPoint2D[] contacts = new ContactPoint2D[20];
+            int points = collision.GetContacts(contacts);
+            vec2 = contacts[0].point;//여기 지우면 아래쪽 오브젝트로
+            Debug.Log("접점 개수" + points);//현재 접점개수가 0개로 나옴
+            for (int i = 0; i < points; i++)
+            {
+                Debug.Log(contacts[i].point);
+            }
+
+            float ysize = gobj.gameObject.GetComponent<BoxCollider2D>().size.y / 2;
+            vec2.y += ysize;
+            Instantiate(gobj, vec2, Quaternion.identity);
+            this.transform.parent.gameObject.SetActive(false);
         }
     }
 
@@ -40,20 +60,22 @@ public class FallingTrap : MonoBehaviour
             joint2D.useConnectedAnchor = false;
             joint2D.connectedBody = null;
         }
-        if (joint2D != null & collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (joint2D != null && collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            this.transform.parent.gameObject.SetActive(false);
-            Vector2 vec2 = this.transform.position;
-            GameObject gobj = transform.parent.GetComponent<TrapSet>().TrapWreck;
-            ContactPoint2D[] contacts = new ContactPoint2D[20];
-            int points = collision.GetContacts(contacts);
-            vec2 = contacts[0].point;
-            Debug.Log("접점 개수" + points);//현재 접점개수가 0개로 나옴
-            for(int i =0;i<points; i++)
-            {
-                Debug.Log(contacts[i].point);
-            }
-            Instantiate(gobj, vec2, Quaternion.Euler(0,0,0));
+            //Vector2 vec2 = this.transform.position;
+            //GameObject gobj = transform.parent.GetComponent<TrapSet>().TrapWreck;
+            ////GameManager.Instance.
+            //ContactPoint2D[] contacts = new ContactPoint2D[20];
+            //int points = collision.GetContacts(contacts);
+            //vec2 = contacts[0].point;//여기 지우면 아래쪽 오브젝트로
+            //Debug.Log("접점 개수" + points);//현재 접점개수가 0개로 나옴
+            //for (int i = 0; i < points; i++)
+            //{
+            //    Debug.Log(contacts[i].point);
+            //}
+            ////this.transform.parent.gameObject.SetActive(false);
+            //Instantiate(gobj, vec2, Quaternion.identity);
+
         }
 
         //bool isDamaged = false;//적에게 바로 충돌하는 함수
