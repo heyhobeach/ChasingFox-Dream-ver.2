707a2a8233f0df5b54bc36630abc7a41496601bd
heyhobeach <zxc120021@gmail.com>
2024-12-01 15:08:07 +0900

    ......

--- Assets/Scripts/TestScripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/TestScripts/Dialogue/DialogueController.cs
@@ -4,11 +4,14 @@ public class DialogueController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public void SetDialogueNum(ScriptableObject scriptorble)//
+    public void SetDialogueNum(DialogueIndex scriptorble)//호출이 안되는듯
     {
         //InteractionEvent.Instance.indexNum = indexNum;
         //InteractionEvent.Instance.num = num;
-        //InteractionEvent.Instance.GetDialogue();
+        InteractionEvent.Instance.indexNum = scriptorble.indexNum;
+        InteractionEvent.Instance.num=scriptorble.num;
+        Debug.Log(InteractionEvent.Instance.indexNum + "" + InteractionEvent.Instance.num);
+        InteractionEvent.Instance.GetDialogue();
     }
 
 
