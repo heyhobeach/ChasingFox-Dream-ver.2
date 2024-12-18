aa1e31ef39bf5895ac9908fadc604362695220c7
heyhobeach <zxc120021@gmail.com>
2024-12-18 14:16:57 +0900

    수집품

--- Assets/Scripts/TestScripts/Collection/CollectionInteractive.cs
+++ Assets/Scripts/TestScripts/Collection/CollectionInteractive.cs
@@ -12,12 +12,12 @@ public class CollectionInteractive : EventTrigger
     public void CallCollectionPopup()
     {
         Collider2D collider = this.gameObject.GetComponent<BoxCollider2D>();
-        Debug.Log("collection의 이름 입니다" + scriptorbleobj._name);
-        Debug.Log("collection의 내용 입니다" + scriptorbleobj._context);
-        Debug.Log("collection의 위치 입니다" + collider.transform.position.x+","+collider.bounds.center.y + collider.bounds.extents.y);
-        Debug.Log("collection의 위치 입니다" + this.gameObject.transform.position);
+        //Debug.Log("collection의 이름 입니다" + scriptorbleobj._name);
+        //Debug.Log("collection의 내용 입니다" + scriptorbleobj._context);
+        //Debug.Log("collection의 위치 입니다" + collider.transform.position.x+","+collider.bounds.center.y + collider.bounds.extents.y);
+        //Debug.Log("collection의 위치 입니다" + this.gameObject.transform.position);
         Vector2 vec=Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
-        Debug.Log("스크린 위치 vec"+vec);
+        //Debug.Log("스크린 위치 vec"+vec);
         CollectionCanvasController.Instance.SetPosition(vec); 
         CollectionCanvasController.Instance.Popup();
 
