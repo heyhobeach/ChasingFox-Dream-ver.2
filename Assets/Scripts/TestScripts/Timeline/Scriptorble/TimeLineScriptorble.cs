e40045092923d895884ef5a1262b56c643f8cd3a
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-12-20 17:15:54 +0900

    ...

--- Assets/Scripts/TestScripts/Timeline/Scriptorble/TimeLineScriptorble.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.Playables;
-using UnityEngine.Timeline;
-
-[CreateAssetMenu(fileName = "TimeLineScriptorble", menuName = "Scriptable Objects/TimeLineScriptorble")]
-public class TimeLineScriptorble : ScriptableObject
-{
-    public GameObject timeline;
-}
