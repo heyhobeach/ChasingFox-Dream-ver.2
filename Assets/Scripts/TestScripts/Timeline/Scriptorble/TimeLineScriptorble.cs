82be4bbacfe3c7f93fedad02cf413f48d7887740
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-12-20 17:09:01 +0900

    수정

---
+++ Assets/Scripts/TestScripts/Timeline/Scriptorble/TimeLineScriptorble.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[CreateAssetMenu(fileName = "TimeLineScriptorble", menuName = "Scriptable Objects/TimeLineScriptorble")]
+public class TimeLineScriptorble : ScriptableObject
+{
+    public GameObject timeline;
+}
