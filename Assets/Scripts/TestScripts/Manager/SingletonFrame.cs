797daea44e700fb2fe7369071ecca51ce3454a34
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2025-01-06 06:04:29 +0900

    Update SingletonFrame.cs

--- Assets/Scripts/TestScripts/Manager/SingletonFrame.cs
+++ Assets/Scripts/TestScripts/Manager/SingletonFrame.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SingletonFrame<T> : MonoBehaviour where T :SingletonFrame<T>
+public class SingletonFrame<T> : MonoBehaviour where T :SingletonFrame<T>//두개의 타입이 동일한지 판단함
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static T instance;
