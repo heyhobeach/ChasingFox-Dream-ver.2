6af261e601821d11a44135a0001d308e75d5b8a1
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2025-02-04 20:58:53 +0900

    프로젝트 셋팅 수정

--- Assets/Scripts/TestScripts/UI/Inventory/ButtonInfo.cs
+++ Assets/Scripts/TestScripts/UI/Inventory/ButtonInfo.cs
@@ -6,14 +6,28 @@ public class ButtonInfo : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public Tuple<int,Inventory.Info> button_info;
-    public Tuple<int,Inventory.News> button_news;
+    public Tuple<int, Inventory.Info> button_info;
+    public Tuple<int, Inventory.News> button_news;
+    GameObject gobj;
 
+    private void Awake()
+    {
+        gobj = this.transform.parent.transform.parent.transform.parent.transform.parent.transform.parent.gameObject;//is_trace 변수 가져오려고
+        //Debug.Log("gobj"+ gobj.name);
+    }
 
     public void SetSelectNum()
     {
-        InventoryController.select_num=this.button_info.Item1;
+        if (gobj.GetComponent<InventoryController>().is_trace)
+        {
+            InventoryController.select_num = this.button_info.Item1;
+        }
+        else
+        {
+            InventoryController.select_num = this.button_news.Item1;
+        }
+
     }
 
-    //public void Setnewsnum()// 여기 내용이 왜 없지?
+    //public void Setnewsnum()
 }
