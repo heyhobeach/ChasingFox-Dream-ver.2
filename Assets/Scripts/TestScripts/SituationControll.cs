d6e92582276e476347340c58697cdaa462b12537
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2025-02-04 21:41:21 +0900

    윙맨 타임 라인 재생
    대화 끝나고 재생 되는것과 신문 재 수집 안되는거 확인
    브런치 작동하는지 확인

--- Assets/Scripts/TestScripts/SituationControll.cs
+++ Assets/Scripts/TestScripts/SituationControll.cs
@@ -2,12 +2,17 @@ using Collection;
 using System.Linq;
 using System.Net.Mail;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class SituationControll : MonoBehaviour
 {
 
+
     public NewsScriptorble[] wingman_news;
+
+    public GameObject WingmanTimelines;
+    public int chapter = 3;
 
     public bool is_get_nesw = false;
 
@@ -22,6 +27,7 @@ public class SituationControll : MonoBehaviour
 
     public Situation situation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
 
@@ -83,19 +89,24 @@ public class SituationControll : MonoBehaviour
     {
         if (!is_get_nesw)
         {
+            InteractionEvent.Instance.move(0);
+            GameObject target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;
+            //target_timeline.GetComponent<TimeLineEnd>().PDPLay();
+            target_timeline.GetComponent<PlayableDirector>().Play();
+
             if (wingman_news == null)
             {
                 Debug.LogError("윙맨 뉴스 없음 뉴스 넣어주세요");
                 return;
             }
 
-            int chp = DatabaseManager.instance.chapter;
-            if (chp < 3)
+            chapter = DatabaseManager.instance.chapter;
+            if (chapter < 3)
             {
                 Debug.LogError("윙맨이 신문을 가지고 있지 않는 챕터 입니다");
                 return;
             }
-            Debug.Log(wingman_news[chp - 3].image);
+            //Debug.Log(wingman_news[chapter - 3].image);
             //UIController.Instance.DialogueCanvasSetTrue();
             //InventoryManager.Instance.AddNews(wingman_news[DatabaseManager.instance.chapter-3]);//될것 같음
             is_get_nesw = true;
@@ -104,6 +115,8 @@ public class SituationControll : MonoBehaviour
         {
             int id =Random.Range(1, 4);//여기 숫자는 윙맨의 대사 id에 따라 달라질 예정입니다
             InteractionEvent.Instance.move(id);
+            GameObject target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;
+            target_timeline.GetComponent<PlayableDirector>().Play();
         }
 
 
