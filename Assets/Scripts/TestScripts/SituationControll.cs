783e4ccb111a1683fd26286c179d874569b4be50
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2025-02-04 23:10:33 +0900

    윙맨 여러번 재생 확인
    여러번 재생은 되나 id에 관해서 생각 해 볼 필요 있음 예외 처리등이 필요함

--- Assets/Scripts/TestScripts/SituationControll.cs
+++ Assets/Scripts/TestScripts/SituationControll.cs
@@ -16,6 +16,8 @@ public class SituationControll : MonoBehaviour
 
     public bool is_get_nesw = false;
 
+    GameObject target_timeline;
+
     public enum Situation
     {
         InventoryBox,
@@ -53,8 +55,9 @@ public class SituationControll : MonoBehaviour
         }
         if (iskeydown)
         {
-            ObjectEvent();
+        
             iskeydown = false;
+            ObjectEvent();
         }
 
     }
@@ -87,36 +90,42 @@ public class SituationControll : MonoBehaviour
 
     public void Receive()
     {
-        if (!is_get_nesw)
+        Debug.Log("Receive");
+        //chapter = DatabaseManager.instance.chapter;//여기 chapter는 나중에 저장된 데이터를 가져올 예정 해
+        target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;//브런치 없는 타임라인
+        if (target_timeline.GetComponent<PlayableDirector>().time == 0)
         {
-            InteractionEvent.Instance.move(0);
-            GameObject target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;
-            //target_timeline.GetComponent<TimeLineEnd>().PDPLay();
-            target_timeline.GetComponent<PlayableDirector>().Play();
-
-            if (wingman_news == null)
+            if (!is_get_nesw)
             {
-                Debug.LogError("윙맨 뉴스 없음 뉴스 넣어주세요");
-                return;
+                InteractionEvent.Instance.move(0);
+
+                //target_timeline.GetComponent<TimeLineEnd>().PDPLay();
+                target_timeline.GetComponent<PlayableDirector>().Play();
+
+                if (wingman_news == null)
+                {
+                    Debug.LogError("윙맨 뉴스 없음 뉴스 넣어주세요");
+                    return;
+                }
+                if (chapter < 3)
+                {
+                    Debug.LogError("윙맨이 신문을 가지고 있지 않는 챕터 입니다");
+                    return;
+                }
+                is_get_nesw = true;
+            }
+            else//선택 이후 신문 받은 경우 타임라인 재생
+            {
+
+                //target_timeline = WingmanTimelines.transform.GetChild(branch).gameObject;//브런치 타임라인을 저장 할 예정 신문 받은 응답에 대한 타임라인이 여러개가 나온다면 해당 타임라인 혹은
+                                                                                           //타임라인 덩어리 target_timeline과 같은걸 넣어서 오브젝트를 가져올 예정
+
+                //int id =Random.Range(1, 4);//여기 숫자는 윙맨의 대사 id에 따라 달라질 예정입니다
+                //InteractionEvent.Instance.move(id);
+                //target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;
+                target_timeline.GetComponent<PlayableDirector>().Play();
             }
 
-            chapter = DatabaseManager.instance.chapter;
-            if (chapter < 3)
-            {
-                Debug.LogError("윙맨이 신문을 가지고 있지 않는 챕터 입니다");
-                return;
-            }
-            //Debug.Log(wingman_news[chapter - 3].image);
-            //UIController.Instance.DialogueCanvasSetTrue();
-            //InventoryManager.Instance.AddNews(wingman_news[DatabaseManager.instance.chapter-3]);//될것 같음
-            is_get_nesw = true;
-        }
-        else
-        {
-            int id =Random.Range(1, 4);//여기 숫자는 윙맨의 대사 id에 따라 달라질 예정입니다
-            InteractionEvent.Instance.move(id);
-            GameObject target_timeline = WingmanTimelines.transform.GetChild(chapter - 3).gameObject;
-            target_timeline.GetComponent<PlayableDirector>().Play();
         }
 
 
